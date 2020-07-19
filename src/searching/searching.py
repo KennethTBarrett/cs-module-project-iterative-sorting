6abe74fd022856e45b1b66814911eaa81427bbc5
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-07-19 17:55:23 -0500

    Linear search section complete

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,16 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
+    # Linear search is super easy!
+    # We just need to iterate through our entire array
+    # And compare the current index to our target.
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1  # Not found.
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
     # Your code here
-
 
     return -1  # not found
